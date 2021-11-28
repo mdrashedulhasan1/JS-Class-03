56bc163eddd257a7b0b90c4a2ad5c2eba4af8136
Rashedul Hasan <Rashedul Hasan>
2021-11-29 00:25:59 +0600

    Loop-Array

---
+++ index.js
@@ -0,0 +1,61 @@
+//Array
+// var friendsAge = [17, 21, 23, 22, 25, 20];
+// friendsAge.push(33);
+// friendsAge.push(31);
+// friendsAge.push(30);
+// console.log(friendsAge);
+// friendsAge.pop();
+// console.log(friendsAge);
+// friendsAge.unshift(15);
+// console.log(friendsAge);
+// friendsAge.shift();
+// console.log(friendsAge);
+// friendsAge[2] = 34;
+// console.log(friendsAge);
+// friendsAge.splice(0,3);
+// console.log(friendsAge);
+// var partOfFriendsAge = friendsAge.slice(3,5);
+// console.log(partOfFriendsAge);
+
+//While Loop
+
+// var i = 21;
+// while(i<=20){
+//     console.log(num);
+//     i++;   //num = num+1;
+// }
+// console.log("Not Found!");
+
+// For Loop
+// var friendsAge = [17, 21, 23, 22, 25, 20];
+// for(var num=0;num<friendsAge.length;num++){
+//     var result = friendsAge[num];
+//     console.log(result);
+// }
+
+//Switch
+
+var num = 3;
+switch(num){
+    // case 1:
+    //     console.log("One");
+    //     break;
+    case 1:
+    case 2:
+    case 3:
+        console.log("Hello World!");
+        break;
+    case 3:
+        console.log("Three");
+        break;
+    case 4:
+        console.log("Four");
+        break;
+    case 5:
+        console.log("Five");
+        break;
+    default:
+        console.log("Not Found!");
+
+}
+
